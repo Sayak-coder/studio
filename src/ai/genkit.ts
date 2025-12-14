e1914efebb7a07f85e37936c8e2eadfc8459d44f
Soumojit Das <soumojitdas08@gmail.com>
2025-12-14 19:52:13 +0000

    its not working fix it

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import {googleAI} from '@genkit-ai/google-genai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: 'AIzaSyDR9MFmvZEBx01-xcG4VfcHX2KBdQrGEVQ',
+      apiKey: process.env.GEMINI_API_KEY,
     }),
   ],
   model: 'googleai/gemini-pro',
